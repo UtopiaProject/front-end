b130805d77f4eefedbc09f9dbcc1075368f8c6d7
Filipe Gorges Reuwsaat <lipirr@hotmail.com>
2018-08-30 00:24:54 -0300

    Add editorconfig and eslint configurations

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "no-param-reassign": 0,
+        "prefer-destructuring": 0,
+    },
+    "globals": {
+        "document": false,
+        "Audio": false,
+    }
+};
